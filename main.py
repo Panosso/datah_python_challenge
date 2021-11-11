f410831f7b1a8111c7369031752d5b923bef04e4
Pedro Panosso Machado <pedropanosso@gmail.com>
2021-11-11 13:08:01 -0300

    Create a suit validation for draw games.

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def python_challenge_datah():
             card_order_dict = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "T": 10, "J": 11,
                                "Q": 12, "K": 13, "A": 14}
 
+            suit_order_dict = {"S": 1, "D": 2, "C": 3, "H": 4}
+
             def check_royal_flush(hand):
                 cards = ''.join([h[0] for h in hand])
                 if cards == "TJQKA":
@@ -74,7 +76,7 @@ def python_challenge_datah():
                 else:
                     return False
 
-            def check_tree_kind(hand):
+            def check_three_kind(hand):
                 card_value = [i[0] for i in hand]
                 value_counts = defaultdict(lambda: 0)
                 for v in card_value:
@@ -109,43 +111,40 @@ def python_challenge_datah():
                 value_dict = defaultdict(lambda: 0)
                 for i in set(card_values):
                     value_dict[card_values.count(i)] = i
-
-                return value_dict[4]
+                return card_order_dict[value_dict[4]]
 
             def check_three_highest_card(hand):
                 card_values = [h[0] for h in hand]
                 value_dict = defaultdict(lambda: 0)
                 for i in set(card_values):
                     value_dict[card_values.count(i)] = i
-
-                return value_dict[3]
+                return card_order_dict[value_dict[3]]
 
             def check_double_pair_highest_card(hand):
                 card_values = [h[0] for h in hand]
-                lista = []
+                card_list = []
                 for card in card_values:
-                    if card_values.count(card) == 2 and card not in lista:
-                        lista.append(card)
+                    if card_values.count(card) == 2 and card not in card_list:
+                        card_list.append(card)
 
-                return card_order_dict[lista[-1]]
+                return card_order_dict[card_list[-1]]
 
             def check_double_pair_highest_solo_card(hand):
                 card_values = [h[0] for h in hand]
-                lista = []
+                card_list = []
                 for card in card_values:
-                    if card_values.count(card) == 1 and card not in lista:
-                        lista.append(card)
+                    if card_values.count(card) == 1 and card not in card_list:
+                        card_list.append(card)
 
-                return card_order_dict[lista[-1]]
+                return card_order_dict[card_list[-1]]
 
             def check_pair_highest_solo_card(hand):
                 card_values = [h[0] for h in hand]
-                lista = []
+                card_list = []
                 for card in card_values:
-                    if card_values.count(card) == 1 and card not in lista:
-                        lista.append(card)
-
-                return card_order_dict[lista[-1]]
+                    if card_values.count(card) == 1 and card not in card_list:
+                        card_list.append(card)
+                return card_order_dict[card_list[-1]]
 
             def check_pair_highest_card(hand):
                 card_values = [h[0] for h in hand]
@@ -171,34 +170,79 @@ def python_challenge_datah():
 
                 return straight_sum
 
+            def get_straight_flush_highest_suit(hand):
+                suit_value = [h[1] for h in hand]
+                suit = list(set(suit_value))
+                return suit_order_dict[suit[0]]
+
+            def get_double_pair_solo_card_highest_suit(hand):
+                card_value = [h[0] for h in hand]
+                card_position = -1
+                for card in card_value:
+                    card_position += 1
+                    if card_value.count(card) == 1:
+                        return suit_order_dict[hand[card_position][1]]
+
+            def get_pair_highest_card_suit(hand):
+                card_value = [h[0] for h in hand]
+                card_position = -1
+                card_list = []
+                for card in card_value:
+                    card_position += 1
+                    if card_value.count(card) == 1:
+                        card_list.append(hand[card_position])
+
+                card_suit = card_list[-1][1]
+                return suit_order_dict[card_suit]
+
             values = dict(zip('23456789TJQKA', range(2, 15)))
             poker_hand_1 = sorted(self.cards(), key=lambda x: values[x[0]])
             poker_hand_2 = sorted(challenge_poker_hand.cards(), key=lambda x: values[x[0]])
 
+            #1 - Royal Straight Flush
             if check_royal_flush(poker_hand_1) or check_royal_flush(poker_hand_2):
-                if check_royal_flush(poker_hand_1):
+
+                if check_royal_flush(poker_hand_1) and check_royal_flush(poker_hand_2):
+
+                    if get_straight_flush_highest_suit(poker_hand_1) > get_straight_flush_highest_suit(poker_hand_2):
+                        return "WIN"
+
+                    else:
+                        return "LOSS"
+
+                elif check_royal_flush(poker_hand_1):
                     return "WIN"
                 else:
                     return "LOSS"
 
+            #2 - Straight Flush
             elif check_straight_flush(poker_hand_1) or check_straight_flush(poker_hand_2):
 
                 if check_straight_flush(poker_hand_1) and check_straight_flush(poker_hand_2):
 
                     if get_highest_card(poker_hand_1) > get_highest_card(poker_hand_2):
                         return "WIN"
+
+                    elif get_highest_card(poker_hand_1) == get_highest_card(poker_hand_2):
+                        if get_straight_flush_highest_suit(poker_hand_1) > get_straight_flush_highest_suit(poker_hand_2):
+                            return "WIN"
+
+                        else:
+                            return "LOSS"
+
                     else:
                         return "LOSS"
 
                 elif check_straight_flush(poker_hand_1):
                     return "WIN"
+
                 else:
                     return "LOSS"
 
+            #3 - Four of a Kind
             elif check_four_kind(poker_hand_1) or check_four_kind(poker_hand_2):
 
                 if check_four_kind(poker_hand_1) and check_four_kind(poker_hand_2):
-
                     if check_four_highest_card(poker_hand_1) > check_four_highest_card(poker_hand_2):
                         return "WIN"
                     else:
@@ -210,6 +254,7 @@ def python_challenge_datah():
                 else:
                     return "LOSS"
 
+            #4 - Full House
             elif check_full_house(poker_hand_1) or check_full_house(poker_hand_2):
 
                 if check_full_house(poker_hand_1) and check_full_house(poker_hand_2):
@@ -225,11 +270,19 @@ def python_challenge_datah():
                 else:
                     return "LOSS"
 
+            #5 - Flush
             elif check_flush(poker_hand_1) or check_flush(poker_hand_2):
 
                 if check_flush(poker_hand_1) and check_flush(poker_hand_2):
                     if get_highest_card(poker_hand_1) > get_highest_card(poker_hand_2):
                         return "WIN"
+
+                    elif get_highest_card(poker_hand_1) == get_highest_card(poker_hand_2):
+                        if get_straight_flush_highest_suit(poker_hand_1) > get_straight_flush_highest_suit(poker_hand_2):
+                            return "WIN"
+
+                        else:
+                            return "LOSS"
 
                     else:
                         return "LOSS"
@@ -240,6 +293,7 @@ def python_challenge_datah():
                 else:
                     return "LOSS"
 
+            #6 - Straight
             elif check_straight(poker_hand_1) or check_straight(poker_hand_2):
 
                 if check_straight(poker_hand_1) and check_straight(poker_hand_2):
@@ -255,9 +309,10 @@ def python_challenge_datah():
                 else:
                     return "LOSS"
 
-            elif check_tree_kind(poker_hand_1) or check_tree_kind(poker_hand_2):
+            #7 - Three of kind
+            elif check_three_kind(poker_hand_1) or check_three_kind(poker_hand_2):
 
-                if check_tree_kind(poker_hand_1) and check_tree_kind(poker_hand_2):
+                if check_three_kind(poker_hand_1) and check_three_kind(poker_hand_2):
 
                     if check_three_highest_card(poker_hand_1) > check_three_highest_card(poker_hand_2):
                         return "WIN"
@@ -265,12 +320,13 @@ def python_challenge_datah():
                     else:
                         return "LOSS"
 
-                elif check_tree_kind(poker_hand_1):
+                elif check_three_kind(poker_hand_1):
                     return "WIN"
 
                 else:
                     return "LOSS"
 
+            #8 - Two Pair
             elif check_two_pair(poker_hand_1) or check_two_pair(poker_hand_2):
 
                 if check_two_pair(poker_hand_1) and check_two_pair(poker_hand_2):
@@ -283,9 +339,19 @@ def python_challenge_datah():
                         if check_double_pair_highest_solo_card(poker_hand_1) > check_double_pair_highest_solo_card(poker_hand_2):
                             return "WIN"
 
-                        else:
+                        elif check_double_pair_highest_solo_card(poker_hand_1) == check_double_pair_highest_solo_card(poker_hand_2):
 
+                            if get_double_pair_solo_card_highest_suit(poker_hand_1) > get_double_pair_solo_card_highest_suit(poker_hand_2):
+                                return "WIN"
+
+                            else:
+                                return "LOSS"
+
+                        else:
                             return "LOSS"
+
+                    else:
+                        return "LOSS"
 
 
                 elif check_two_pair(poker_hand_1):
@@ -294,9 +360,11 @@ def python_challenge_datah():
                 else:
                     return "LOSS"
 
+            #9 - One Pair
             elif check_one_pairs(poker_hand_1) or check_one_pairs(poker_hand_2):
 
                 if check_one_pairs(poker_hand_1) and check_one_pairs(poker_hand_2):
+
                     if check_pair_highest_card(poker_hand_1) > check_pair_highest_card(poker_hand_2):
                         return "WIN"
 
@@ -304,6 +372,15 @@ def python_challenge_datah():
 
                         if check_pair_highest_solo_card(poker_hand_1) > check_pair_highest_solo_card(poker_hand_2):
                             return "WIN"
+
+                        elif check_pair_highest_solo_card(poker_hand_1) == check_pair_highest_solo_card(poker_hand_2):
+
+                            if get_pair_highest_card_suit(poker_hand_1) > get_pair_highest_card_suit(poker_hand_2):
+                                return "WIN"
+
+                            else:
+                                return "LOSS"
+
                         else:
 
                             return "LOSS"
@@ -317,13 +394,91 @@ def python_challenge_datah():
                 else:
                     return "LOSS"
 
+            #10 - High Card
             else:
                 if get_highest_card(poker_hand_1) > get_highest_card(poker_hand_2):
                     return "WIN"
 
+                elif get_highest_card(poker_hand_1) == get_highest_card(poker_hand_2):
+
+                    if get_pair_highest_card_suit(poker_hand_1) > get_pair_highest_card_suit(poker_hand_2):
+                        return "WIN"
+
+                    else:
+                        return "LOSS"
+
                 else:
                     return "LOSS"
 
+    #Test -> Must be 1º:WIN,  2º:LOSS
+
+    #1
+    print("Check Royal Straight Flush:")
+    print(PokerHand("TH JH QH KH AH").compare_with(PokerHand("TC JC QC KC AC")))
+    print(PokerHand("TC JC QC KC AC").compare_with(PokerHand("TH JH QH KH AH")))
+
+    #2
+    print("\nCheck Straight Flush:")
+    print(PokerHand("AH 2H 3H 4H 5H").compare_with(PokerHand("AC 2C 3C 4C 5C")))
+    print(PokerHand("AC 2C 3C 4C 5C").compare_with(PokerHand("AH 2H 3H 4H 5H")))
+
+    #3
+    print("\nCheck four of kind:")
+    print(PokerHand("AC AD AS AH 5H").compare_with(PokerHand("KC KS KH KD 5C")))
+    print(PokerHand("KC KS KH KD 5C").compare_with(PokerHand("AC AD AS AH 5H")))
+
+    #4
+    print("\nCheck full house:")
+    print(PokerHand("QC QD QS 5H 5H").compare_with(PokerHand("JC JS JH 5D 5C")))
+    print(PokerHand("JC JS JH 5D 5C").compare_with(PokerHand("QC QD QS 5H 5H")))
+
+    #5
+    print("\nCheck flush:")
+    print(PokerHand("2H 4H 6H 5H KH").compare_with(PokerHand("2C 4C 6C 5C KC")))
+    print(PokerHand("2C 4C 6C 5C KC").compare_with(PokerHand("2H 4H 6H 5H KH")))
+
+    #6
+    print("\nCheck flush:")
+    print(PokerHand("2C 3D 4H 5D 6S").compare_with(PokerHand("AC 2D 3C 4D 5S")))
+    print(PokerHand("AC 2D 3C 4D 5S").compare_with(PokerHand("2C 3D 4H 5D 6S")))
+
+    #7
+    print("\nThree of a kind:")
+    print(PokerHand("JC JD JH AD 9S").compare_with(PokerHand("6C 6D 6H 8D 7S")))
+    print(PokerHand("6C 6D 6H 8D 7S").compare_with(PokerHand("7C 7D 7H AD 9S")))
+
+    #8
+    print("\nTwo pair:")
+    print(PokerHand("7C 7D AH AD 9S").compare_with(PokerHand("6C 6D 8H 8D 7S")))
+    print(PokerHand("6C 6D 8H 8D 7S").compare_with(PokerHand("7C 7D 7H AD 9S")))
+    print("\nTwo pair, 5º card win:")
+    print(PokerHand("6C 6D 8C 8D 7H").compare_with(PokerHand("6H 6S 8H 8S 7S")))
+    print(PokerHand("6H 6S 8H 8S 7S").compare_with(PokerHand("6C 6D 8C 8D 7H")))
+
+    #9
+    print("\nOne pair:")
+    print(PokerHand("7C 7D AH 8D 9S").compare_with(PokerHand("6C 6D 8H 9D 7S")))
+    print(PokerHand("6C 6D 8H 9D 7S").compare_with(PokerHand("7C 7D AH 8D 9S")))
+
+    print("\nOne pair highest card win:")
+    print(PokerHand("7C 7D AH 8D 9S").compare_with(PokerHand("7S 7H KS 9D 6S")))
+    print(PokerHand("7S 7H KS 9D 6S").compare_with(PokerHand("7C 7D AH 8D 9S")))
+
+    print("\nOne pair highest suit win:")
+    print(PokerHand("7C 7D AH 8D 9S").compare_with(PokerHand("7S 7H AS 8D 9S")))
+    print(PokerHand("7S 7H AS 8D 9S").compare_with(PokerHand("7C 7D AH 8D 9S")))
+
+    #10
+    print("\nHigh card:")
+    print(PokerHand("7C 5D AH 8D 9S").compare_with(PokerHand("6C 3D 8H 9D 7S")))
+    print(PokerHand("6C 3D 8H 9D 7S").compare_with(PokerHand("7C 5D AH 8D 9S")))
+
+    print("\nHigh suit:")
+    print(PokerHand("7C 2D AH 8D 9S").compare_with(PokerHand("7S 2D AS 8C 9D")))
+    print(PokerHand("7S 2D AS 8C 9D").compare_with(PokerHand("7C 2D AH 8D 9S")))
+
+
+    print("\nPDF TEST:")
     print(PokerHand("JH JC JS JD TH").compare_with(PokerHand("7C 8S 9H TH JH")))
 
     print(PokerHand("TS TD KC JC 7C").compare_with(PokerHand("JS JC AS KC TD")))
